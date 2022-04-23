dca48f0f93f7d5ade0e887ba00f245bd6a21f789
dragg54 <ajibolasadiq@yahoo.com>
2022-04-23 02:44:38 +0100

    mobile screen development

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,22 @@ module.exports = {
      
       height:{
         '720': '48rem',
-        'mobile-height':'500px'
+        'mobile-height':'550px',
+        '600px': '550px',
+        '500px' : '500px',
+        '520px' :'520px',
+        '760px' : '760px',
+        '660px' : '660px',
+        '200px' : '200px',
+        '300px' : '300px',
+        '900px' : '900px',
+        '85rem' : '72rem',
+        '10rem' : '10rem',
+        '32rem' : '32rem',
+        'mobile-box': '25rem',
+        '40rem' : '40rem',
+        '400px': '400px',
+        '1000px' : '1000px'
       },
 
       fontSize:{
@@ -26,13 +41,29 @@ module.exports = {
         'Oleo': 'Syne Tactile',
         'Yeseva': 'Yeseva One',
         'Eczar': 'Eczar',
-        'Playfair': 'Playfair Display'
+        'Playfair': 'Playfair Display',
+        'Poppins' : 'Poppins'
         
       },
       
       width:{
-        'so-wide':'75rem'
+        'so-wide':'75rem',
+        '37.5rem' : '37.5rem',
+        '240px' : '240px',
+        '750px' : '750px',
+        '220px' : '220px', 
+        '400px' : '400px',
+        '300px' : '300px',
+        form : '22rem',
+        'mobile-box': '13rem',
+        'mobile-container' : '15rem',
+        'container' : '17rem',
+        '32rem' : '32rem'
       },
+
+      margin:{
+        'drop': '200px'
+      }
       
     },
   },
