2cc2eaac9b74028231b8e516d6b73f5ff1f11898
dragg54 <ajibolasadiq@yahoo.com>
2022-04-23 19:11:33 +0100

    index

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,75 +1,73 @@
 module.exports = {
-    content: [
-      './pages/**/*.{html,js}',
-      './components/**/*.{html,js}',
-      './index.html'
-    ],
+  content: [
+    "./pages/**/*.{html,js}",
+    "./components/**/*.{html,js}",
+    "./public/index.html",
+  ],
   theme: {
     extend: {
-      colors:{
-        magenta: '#EC8FD0'
-      }, 
+      colors: {
+        magenta: "#EC8FD0",
+      },
       backgroundImage: {
-        'furniture': "url('../public/images/furniture.jpg')",
+        furniture: "url('../public/images/furniture.jpg')",
       },
 
       boxShadow: {
-        'overshadow': '2px 2px 2px pink'
-      },
-     
-      height:{
-        '720': '48rem',
-        'mobile-height':'550px',
-        '600px': '550px',
-        '500px' : '500px',
-        '520px' :'520px',
-        '760px' : '760px',
-        '660px' : '660px',
-        '200px' : '200px',
-        '300px' : '300px',
-        '900px' : '900px',
-        '85rem' : '72rem',
-        '10rem' : '10rem',
-        '32rem' : '32rem',
-        'mobile-box': '25rem',
-        '40rem' : '40rem',
-        '400px': '400px',
-        '1000px' : '1000px'
+        overshadow: "2px 2px 2px pink",
       },
 
-      fontSize:{
-        'so-big':'100px'
+      height: {
+        720: "48rem",
+        "mobile-height": "550px",
+        "600px": "550px",
+        "500px": "500px",
+        "520px": "520px",
+        "760px": "760px",
+        "660px": "660px",
+        "200px": "200px",
+        "300px": "300px",
+        "900px": "900px",
+        "85rem": "72rem",
+        "10rem": "10rem",
+        "32rem": "32rem",
+        "mobile-box": "25rem",
+        "40rem": "40rem",
+        "400px": "400px",
+        "1000px": "1000px",
       },
 
-      fontFamily:{
-        'Oleo': 'Syne Tactile',
-        'Yeseva': 'Yeseva One',
-        'Eczar': 'Eczar',
-        'Playfair': 'Playfair Display',
-        'Poppins' : 'Poppins'
-        
-      },
-      
-      width:{
-        'so-wide':'75rem',
-        '37.5rem' : '37.5rem',
-        '240px' : '240px',
-        '750px' : '750px',
-        '220px' : '220px', 
-        '400px' : '400px',
-        '300px' : '300px',
-        form : '22rem',
-        'mobile-box': '13rem',
-        'mobile-container' : '15rem',
-        'container' : '17rem',
-        '32rem' : '32rem'
+      fontSize: {
+        "so-big": "100px",
       },
 
-      margin:{
-        'drop': '200px'
-      }
-      
+      fontFamily: {
+        Oleo: "Syne Tactile",
+        Yeseva: "Yeseva One",
+        Eczar: "Eczar",
+        Playfair: "Playfair Display",
+        Poppins: "Poppins",
+      },
+
+      width: {
+        "so-wide": "75rem",
+        "37.5rem": "37.5rem",
+        "240px": "240px",
+        "750px": "750px",
+        "220px": "220px",
+        "400px": "400px",
+        "300px": "300px",
+        form: "22rem",
+        "mobile-box": "13rem",
+        "mobile-container": "15rem",
+        container: "17rem",
+        "32rem": "32rem",
+      },
+
+      margin: {
+        drop: "200px",
+      },
     },
   },
   plugins: [],
-}
+};
